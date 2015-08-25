a6e73c4239e5b702788b9d5a84454e74ec33a4d8
antonio <antonio@antonio-PC>
2015-08-24 23:27:46 -0300

    As classes DAO, Controller e Bean de pessoas estão criadas porém estou tendo problemas para trbalhar com a data de nascimento da pessoa. Esse problema é a principal coisa que precisa ser corrigida neste momento.

--- Colchonete/src/java/br/com/colchonete/Controller/PessoaController.java
+++ Colchonete/src/java/br/com/colchonete/Controller/PessoaController.java
@@ -1,9 +1,82 @@
 package br.com.colchonete.Controller;
 
+import br.com.colchonete.Bean.PessoaBean;
+import br.com.colchonete.Dao.PessoaDao;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
 /**
  *
  * @author antonio
  */
-public class PessoaController {
+@ManagedBean
+@SessionScoped
+public class PessoaController implements Serializable{
+    private PessoaBean pessoaB;
+    private DataModel listaPessoas;
+
+    public PessoaController() {
+        pessoaB = new PessoaBean();
+    }
+
+    public PessoaBean getPessoaB() {
+        return pessoaB;
+    }
+
+    public void setPessoaB(PessoaBean pessoaB) {
+        this.pessoaB = pessoaB;
+    }
+
+    public DataModel getListaPessoas() {
+        PessoaDao pd = new PessoaDao();
+        listaPessoas = new ListDataModel(pd.listarPessoas());
+        return listaPessoas;
+    }
+
+    public void setListaPessoas(DataModel listaPessoas) {
+        this.listaPessoas = listaPessoas;
+    }
+    
+    public void novaPessoa(){
+        pessoaB = new PessoaBean();
+    }
+    
+    public String salvarPessoa(){
+        PessoaDao pd = new PessoaDao();
+        if(pd.salvarPessoa(pessoaB)){
+            FacesContext contexto = FacesContext.getCurrentInstance();
+            contexto.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastro realizado com sucesso!",null) );
+            return "pagina_principal";
+        }else{
+            return "error";
+        }
+    }
+    
+    public String editarPessoa(){
+        PessoaDao pd = new PessoaDao();
+        if(pd.editarPessoa(pessoaB)){
+            FacesContext contexto = FacesContext.getCurrentInstance();
+            contexto.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastro editado com sucesso!",null) );
+            return "pagina_principal";
+        }else{
+            return "error";
+        }
+    }
+    
+    public String excluirPessoa(){
+        PessoaDao pd = new PessoaDao();
+        if(pd.excluirPessoa(pessoaB)){
+            FacesContext contexto = FacesContext.getCurrentInstance();
+            contexto.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastro excluido com sucesso!",null) );
+            return "pagina_principal";
+        }else{
+            return "error";
+        }
+    }
     
 }
--- Colchonete/src/java/br/com/colchonete/Dao/PessoaDao.java
+++ Colchonete/src/java/br/com/colchonete/Dao/PessoaDao.java
@@ -1,9 +1,117 @@
 package br.com.colchonete.Dao;
 
+import br.com.colchonete.Bean.PessoaBean;
+import br.com.colchonete.Utilitarios.Conexao;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author antonio
  */
 public class PessoaDao {
+    private Conexao con;
+
+    public PessoaDao() {
+        con = new Conexao();
+    }
+    
+    public  boolean salvarPessoa(PessoaBean pessoa){
+        try {
+            String sql = "INSERT INTO colchonete_pessoas (nome, nick_name, dt_nascimento, rg, cpf, telefone, email)" + 
+                         "VALUES (?, ?, ?, ?, ?, ?, ?);";
+            System.out.println(pessoa.getDt_nascimento()+"<-AQUI");
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            stm.setString(1, pessoa.getNome());
+            stm.setString(2, pessoa.getNick_name());
+            stm.setDate(3, (Date) pessoa.getDt_nascimento());
+            stm.setInt(4, pessoa.getRg());
+            stm.setInt(5, pessoa.getCpf());
+            stm.setString(6, pessoa.getTelefone());
+            stm.setString(7, pessoa.getEmail());
+            stm.execute();
+            con.getConnection().commit();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public boolean excluirPessoa(PessoaBean pessoa){
+        try {
+            String sql = "DELETE FROM colchonete_pessoas WHERE id_pessoa=?";
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            stm.setInt(1, pessoa.getId_pessoa());
+            stm.execute();
+            con.getConnection().commit();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public boolean editarPessoa(PessoaBean pessoa){
+        try {
+            String sql = "UPDATE colchonete_pessoas SET nome=?, nick_name=?, dt_nascimento=?, rg=?, cpf=?, telefone=?, email=?" + 
+                         "WHERE id_pessoa=?";
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            stm.setString(1, pessoa.getNome());
+            stm.setString(2, pessoa.getNick_name());
+            stm.setDate(3, (Date) pessoa.getDt_nascimento());
+            stm.setInt(4, pessoa.getRg());
+            stm.setInt(5, pessoa.getCpf());
+            stm.setString(6, pessoa.getTelefone());
+            stm.setString(7, pessoa.getEmail());
+            stm.setInt(8, pessoa.getId_pessoa());
+            stm.execute();
+            con.getConnection().commit();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public List<PessoaBean> listarPessoas(){
+        List<PessoaBean> lista = new ArrayList<PessoaBean>();
+        try {
+            String sql = "SELECT * FROM colchonete_pessoas";
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            ResultSet rs = stm.executeQuery();
+            
+            while(rs.next()){
+                PessoaBean pessoa = new PessoaBean();
+                pessoa.setId_pessoa(rs.getInt("id_pessoa"));
+                pessoa.setNome(rs.getString("nome"));
+                pessoa.setNick_name(rs.getString("nick_name"));
+                pessoa.setDt_nascimento(rs.getDate("dt_nascimento"));
+                pessoa.setRg(rs.getInt("rg"));
+                pessoa.setCpf(rs.getInt("cpf"));
+                pessoa.setTelefone(rs.getString("telefone"));
+                pessoa.setEmail(rs.getString("email"));
+                lista.add(pessoa);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return lista;
+    }
     
 }

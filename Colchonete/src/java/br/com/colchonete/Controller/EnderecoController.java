e665ae67443d8f6488f17e98e3b675dd18fa59e7
antonio <antonio@antonio-PC>
2015-08-25 19:49:06 -0300

    Classes de pessoa e endereco estão finalizadas. Neste momento estou criando a view de endereco

--- Colchonete/src/java/br/com/colchonete/Controller/EnderecoController.java
+++ Colchonete/src/java/br/com/colchonete/Controller/EnderecoController.java
@@ -1,9 +1,73 @@
 package br.com.colchonete.Controller;
 
+import br.com.colchonete.Bean.EnderecoBean;
+import br.com.colchonete.Dao.EnderecoDao;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
 /**
  *
  * @author antonio
  */
-public class EnderecoController {
+@ManagedBean
+@SessionScoped
+public class EnderecoController implements Serializable{
+    private EnderecoBean enderecoB;
+    private DataModel listaEnderecos;
+
+    public EnderecoController() {
+        enderecoB = new EnderecoBean();
+    }
+
+    public EnderecoBean getEnderecoB() {
+        return enderecoB;
+    }
+
+    public void setEnderecoB(EnderecoBean enderecoB) {
+        this.enderecoB = enderecoB;
+    }
+
+    public DataModel getListaEnderecos() {
+        EnderecoDao ed = new EnderecoDao();
+        listaEnderecos = new ListDataModel(ed.listarEndereco());
+        return listaEnderecos;
+    }
+
+    public void setListaEnderecos(DataModel listaEnderecos) {
+        this.listaEnderecos = listaEnderecos;
+    }
     
+    public void novoEndereco(){
+        enderecoB = new EnderecoBean();
+    }
+    
+    public String salvarEndereco(){
+        EnderecoDao ed = new EnderecoDao();
+        if(ed.salvarEndereco(enderecoB)){
+            return "pagina_principal";
+        }else{
+            return "error";
+        }
+    }
+    
+    public String editarPessoa(){
+        EnderecoDao ed = new EnderecoDao();
+        if(ed.editarEndereco(enderecoB)){
+            return "pagina_principal";
+        }else{
+            return "error";
+        }
+    }
+    
+    public String excluirPessoa(){
+        EnderecoDao ed = new EnderecoDao();
+        if(ed.excluirEndereco(enderecoB)){
+            return "pagina_principal";
+        }else{
+            return "error";
+        }
+    }
 }
--- Colchonete/src/java/br/com/colchonete/Dao/EnderecoDao.java
+++ Colchonete/src/java/br/com/colchonete/Dao/EnderecoDao.java
@@ -1,9 +1,122 @@
 package br.com.colchonete.Dao;
 
+import br.com.colchonete.Bean.EnderecoBean;
+import br.com.colchonete.Bean.PessoaBean;
+import br.com.colchonete.Utilitarios.Conexao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author antonio
  */
 public class EnderecoDao {
+    private Conexao con;
+
+    public EnderecoDao() {
+    }
     
+    public  boolean salvarEndereco(EnderecoBean endereco){
+        
+        try {
+        
+            String sql = "INSERT INTO colchonete_enderecos (id_pessoa, estado, cidade, bairro, rua, cep,complemento)" + 
+                         "VALUES (?, ?, ?, ?, ?, ?, ?);";
+            
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            stm.setInt(1, endereco.getId_pessoa());
+            stm.setInt(2, endereco.getEstado());
+            stm.setString(3, endereco.getCidade());
+            stm.setString(4, endereco.getBairro());
+            stm.setString(5, endereco.getRua());
+            stm.setString(6, endereco.getCep());
+            stm.setString(7, endereco.getComplemento());
+            stm.execute();
+            
+            con.getConnection().commit();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public boolean excluirEndereco(EnderecoBean endereco){
+        try {
+            String sql = "DELETE FROM colchonete_enderecos WHERE id_endereco=?";
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            stm.setInt(1, endereco.getId_endereco());
+            stm.execute();
+            con.getConnection().commit();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public boolean editarEndereco(EnderecoBean endereco){
+        
+        try {
+        
+            String sql = "UPDATE colchonete_enderecos SET estado=?, cidade=?, bairro=?, rua=?, cep=?, complemento=?" + 
+                         "WHERE id_endereco=?";
+            
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            stm.setInt(1, endereco.getEstado());
+            stm.setString(2, endereco.getCidade());
+            stm.setString(3, endereco.getBairro());
+            stm.setString(4, endereco.getRua());
+            stm.setString(5, endereco.getCep());
+            stm.setString(6, endereco.getComplemento());
+            stm.setInt(5, endereco.getId_endereco());
+            stm.execute();
+            con.getConnection().commit();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public List<EnderecoBean> listarEndereco(){
+        List<EnderecoBean> lista = new ArrayList<EnderecoBean>();
+        try {
+            String sql = "SELECT * FROM colchonete_enderecos";
+            PreparedStatement stm = con.getConnection().prepareStatement(sql);
+            
+            ResultSet rs = stm.executeQuery();
+            
+            while(rs.next()){
+                EnderecoBean endereco = new EnderecoBean();
+                endereco.setId_endereco(rs.getInt("id_endereco"));
+                endereco.setId_pessoa(rs.getInt("id_pessoa"));
+                endereco.setEstado(rs.getInt("estado"));
+                endereco.setCidade(rs.getString("cidade"));
+                endereco.setBairro(rs.getString("bairro"));
+                endereco.setRua(rs.getString("rua"));
+                endereco.setCep(rs.getString("cep"));
+                endereco.setComplemento(rs.getString("complemento"));
+                lista.add(endereco);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return lista;
+    }
 }
